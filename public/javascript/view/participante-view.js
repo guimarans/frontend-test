b7d8295900000009b5df74df5aabb3342304fa1c
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-11 00:40:33 -0300

    style: header pronto e inicio do ranking

--- public/javascript/view/participante-view.js
+++ public/javascript/view/participante-view.js
@@ -6,34 +6,34 @@ export class ParticipanteView extends View {
         ${model.lista().map((participante) => {
             return `
             <div class="ranking__item tooltip">
-                <div class="row">
+                <div class="ranking__content">
                     <figure class="ranking__picture">
-                    <img src="${participante.picture}" alt="${participante.name}" loading="lazy">
-                    <figcaption class="ranking__picture--position">
-                        ${posicao++}
-                    </figcaption>
+                        <img src="${participante.picture}" alt="${participante.name}" loading="lazy">
+                        <figcaption class="ranking__bullet">
+                            ${posicao++}
+                        </figcaption>
                     </figure>
                     <div class="ranking__info">
-                    <h2> ${participante.name}</h2>
-                    <p> ${participante.description} </p>
+                        <h2> ${participante.name}</h2>
+                        <p> ${participante.description} </p>
                     </div>
                 </div>
 
                 <table class="tooltip__content">
                     <thead>
-                    <tr>
-                        <th>Gostam</th>
-                        <th>Nao gostam</th>
-                    </tr>
+                        <tr>
+                            <th>Gostam</th>
+                            <th>Nao gostam</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>${(participante.porcentagemPositiva || 0).toFixed(0)}%</td>
-                        <td>${(participante.porcentagemNegativa || 0).toFixed(0)}%</td>
-                    </tr>
+                        <tr>
+                            <td>${(participante.porcentagemPositiva || 0).toFixed(0)}%</td>
+                            <td>${(participante.porcentagemNegativa || 0).toFixed(0)}%</td>
+                        </tr>
                     </tbody>
                 </table>
-                </div>
+            </div>
             `;
         }).join('')}`;
     }
